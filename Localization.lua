343ed7439a56b448c5f69c03d8eebd37eea9cb3b
Thomas Striegl <pldcanfly@gmail.com>
2019-08-25 03:46:37 +0200

    Added russian translation
    Provided by Horgul-Gordunni

--- Localization.lua
+++ Localization.lua
@@ -39,4 +39,32 @@ if GetLocale() == "deDE" then
  SimpleAutoCombatLog.L["Lord Kazzak"] = "Lord Kazzak";
  SimpleAutoCombatLog.L["Azuregos"] = "Azuregos";
 
+elseif GetLocale() == "ruRU" then -- Translation by Horgul-Gordunni
+  -- Zones
+ SimpleAutoCombatLog.L["The Molten Core"] = "Огненные Недра";
+ SimpleAutoCombatLog.L["Onyxia's Lair"] = "Логово Ониксии";
+ SimpleAutoCombatLog.L["Blackwing Lair"] = "Логово Крыла Тьмы";
+ SimpleAutoCombatLog.L["Ruins of Ahn'Qiraj"] = "Руины Ан'Киража";
+ SimpleAutoCombatLog.L["Ahn'Qiraj"] = "Храм Ан'Киража";
+ SimpleAutoCombatLog.L["Zul'Gurub"] = "Зул'Гуруб";
+ SimpleAutoCombatLog.L["Naxxramas"] = "Наксрамас";
+
+ SimpleAutoCombatLog.L["Azshara"] = "Азшара";
+ SimpleAutoCombatLog.L["Blasted Lands"] = "Выжженные земли";
+ SimpleAutoCombatLog.L["Hinterlands"] = "Внутренние земли";
+ SimpleAutoCombatLog.L["Ashenvale"] = "Ясеневый лес";
+ SimpleAutoCombatLog.L["Feralas"] = "Фералас";
+ SimpleAutoCombatLog.L["Duskwood"] = "Сумеречный лес";
+
+ -- Text
+ SimpleAutoCombatLog.L["Combatlog enabled"] = "Журнал Боя включен";
+ SimpleAutoCombatLog.L["Combatlog disabled"] = "Журнал Боя отключен";
+
+ -- Mobs
+ SimpleAutoCombatLog.L["Emeriss"] = "Эмерисс";
+ SimpleAutoCombatLog.L["Lethon"] = "Летон";
+ SimpleAutoCombatLog.L["Ysondre"] = "Исондра";
+ SimpleAutoCombatLog.L["Taerar"] = "Таэрар";
+ SimpleAutoCombatLog.L["Lord Kazzak"] = "Владыка Каззак";
+ SimpleAutoCombatLog.L["Azuregos"] = "Азурегос";
 end
