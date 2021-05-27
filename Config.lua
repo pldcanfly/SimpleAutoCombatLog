afb4ac7ab88feec8d867f4f444cc7d9dc179b70c
Thomas Striegl <pldcanfly@gmail.com>
2021-05-27 10:18:51 +0200

    toc bump

--- Config.lua
+++ Config.lua
@@ -11,15 +11,6 @@ SimpleAutoCombatLog.Zones = {
     L["Ahn'Qiraj"],
     L["Zul'Gurub"],
     L["Naxxramas"],
-    L["Karazhan"],
-    L["Gruul's Lair"],
-    L["Magtheridon's Lair"],
-    L["Sunwell Plateau"],
-    L["Black Temple"],
-    L["Tempest Keep"],
-    L["Serpentshrine Cavern"],
-    L["Zul'Aman"],
-    L["Hyjal Summit"]
   },
   ["ZoneAndMob"] = { -- A Zone must be entered and a Mob must be targeted
     ["Zones"] = {
@@ -29,8 +20,6 @@ SimpleAutoCombatLog.Zones = {
       L["Ashenvale"],
       L["Blasted Lands"],
       L["Azshara"],
-      L["Hellfire Peninsula"],
-      L["Shadowmoon Valley"]
     },
     ["Mobs"] = {
       L["Emeriss"],
@@ -39,8 +28,6 @@ SimpleAutoCombatLog.Zones = {
       L["Taerar"],
       L["Doomlord Kazzak"],
       L["Azuregos"],
-      L["Doom Lord Kazzak"],
-      L["Doomwalker"]
     }
   }
 
--- Localization.lua
+++ Localization.lua
@@ -19,15 +19,6 @@ if GetLocale() == "deDE" then
  SimpleAutoCombatLog.L["Ahn'Qiraj"] = "Ahn'Qiraj";
  SimpleAutoCombatLog.L["Zul'Gurub"] = "Zul'Gurub";
  SimpleAutoCombatLog.L["Naxxramas"] = "Naxxramas";
- SimpleAutoCombatLog.L["Karazhan"] = "Karazhan";
- SimpleAutoCombatLog.L["Gruul's Lair"] = "Gruuls Unterschlupf";
- SimpleAutoCombatLog.L["Magtheridon's Lair"] = "Magtheridons Kammer";
- SimpleAutoCombatLog.L["Sunwell Plateau"] = "Sonnenbrunnenplateau";
- SimpleAutoCombatLog.L["Black Temple"] = "Der Schwarze Tempel";
- SimpleAutoCombatLog.L["Tempest Keep"] = "Festung der Stürme";
- SimpleAutoCombatLog.L["Serpentshrine Cavern"] = "Höhle des Schlangenschreins";
- SimpleAutoCombatLog.L["Zul'Aman"] = "Zul'Aman";
- SimpleAutoCombatLog.L["Hyjal Summit"] = "Hyjalgipfel";
 
 
  SimpleAutoCombatLog.L["Azshara"] = "Azshara";
@@ -36,8 +27,6 @@ if GetLocale() == "deDE" then
  SimpleAutoCombatLog.L["Ashenvale"] = "Ashenvale";
  SimpleAutoCombatLog.L["Feralas"] = "Feralas";
  SimpleAutoCombatLog.L["Duskwood"] = "Dämmerwald";
- SimpleAutoCombatLog.L["Hellfire Peninsula"] = "Höllenfeuerhalbinsel";
- SimpleAutoCombatLog.L["Shadowmoon Valley"] = "Schattenmondtal";
 
  -- Text
  SimpleAutoCombatLog.L["Combatlog enabled"] = "Kampflog aktiviert";
@@ -50,8 +39,6 @@ if GetLocale() == "deDE" then
  SimpleAutoCombatLog.L["Taerar"] = "Taerar";
  SimpleAutoCombatLog.L["Doomlord Kazzak"] = "Lord Kazzak";
  SimpleAutoCombatLog.L["Azuregos"] = "Azuregos";
- SimpleAutoCombatLog.L["Doom Lord Kazzak"] = "Verdammnislord Kazzak";
- SimpleAutoCombatLog.L["Doomwalker"] = "Verdammniswandler";
 
 elseif GetLocale() == "ruRU" then -- Translation by Horgul-Gordunni
   -- Zones
@@ -62,15 +49,6 @@ elseif GetLocale() == "ruRU" then -- Translation by Horgul-Gordunni
  SimpleAutoCombatLog.L["Ahn'Qiraj"] = "Храм Ан'Киража";
  SimpleAutoCombatLog.L["Zul'Gurub"] = "Зул'Гуруб";
  SimpleAutoCombatLog.L["Naxxramas"] = "Наксрамас";
- SimpleAutoCombatLog.L["Karazhan"] = "Каражан";
- SimpleAutoCombatLog.L["Gruul's Lair"] = "Логово Груула";
- SimpleAutoCombatLog.L["Magtheridon's Lair"] = "Логово Магтеридона";
- SimpleAutoCombatLog.L["Sunwell Plateau"] = "Плато Солнечного Колодца";
- SimpleAutoCombatLog.L["Black Temple"] = "Черный храм";
- SimpleAutoCombatLog.L["Tempest Keep"] = "Крепость Бурь";
- SimpleAutoCombatLog.L["Serpentshrine Cavern"] = "Змеиное святилище";
- SimpleAutoCombatLog.L["Zul'Aman"] = "Зул'Аман";
- SimpleAutoCombatLog.L["Hyjal Summit"] = "Вершина Хиджала";
 
  SimpleAutoCombatLog.L["Azshara"] = "Азшара";
  SimpleAutoCombatLog.L["Blasted Lands"] = "Выжженные земли";
@@ -78,8 +56,6 @@ elseif GetLocale() == "ruRU" then -- Translation by Horgul-Gordunni
  SimpleAutoCombatLog.L["Ashenvale"] = "Ясеневый лес";
  SimpleAutoCombatLog.L["Feralas"] = "Фералас";
  SimpleAutoCombatLog.L["Duskwood"] = "Сумеречный лес";
- SimpleAutoCombatLog.L["Hellfire Peninsula"] = "Полуостров Адского Пламени";
- SimpleAutoCombatLog.L["Shadowmoon Valley"] = "Долина Призрачной Луны";
 
  -- Text
  SimpleAutoCombatLog.L["Combatlog enabled"] = "Журнал Боя включен";
@@ -92,6 +68,4 @@ elseif GetLocale() == "ruRU" then -- Translation by Horgul-Gordunni
  SimpleAutoCombatLog.L["Taerar"] = "Таэрар";
  SimpleAutoCombatLog.L["Doomlord Kazzak"] = "Владыка Каззак";
  SimpleAutoCombatLog.L["Azuregos"] = "Азурегос";
- SimpleAutoCombatLog.L["Doom Lord Kazzak"] = "Владыка судеб Каззак";
- SimpleAutoCombatLog.L["Doomwalker"] = "Судьболом";
 end
